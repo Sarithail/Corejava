80c33a64935a7c54bc31645e7879e19c13efa3a5
sarit <sarit@LAPTOP-MSJC0A3I>
2022-10-15 18:30:27 +0530

    Multiple java

---
+++ CoreJava/src/org/rgit/lesson1/multi.java
@@ -0,0 +1,42 @@
+package org.rgit.lesson1;
+
+class Parent_one
+{
+	void displ1()
+	{
+	System.out.println("I am from parent_onr");
+}
+}
+class Parent_two extends Parent_one
+{   
+	void displ2()
+	{
+	System.out.println("I am from parent_two");
+	}
+}
+class Parent_three extends Parent_two
+{
+	void displ3()
+	{
+	System.out.println("I am from parent_three");
+    }
+}
+class Parent_four extends Parent_three
+{
+	void displ4()
+	{
+	System.out.println("I am from parent_four");
+    }
+}
+public class multi extends Parent_four
+{
+	public static void main(String[] args)
+	{
+		multi obj=new multi();
+		obj.displ1();
+		obj.displ2();
+		obj.displ3();
+		obj.displ4();
+	}
+
+}
